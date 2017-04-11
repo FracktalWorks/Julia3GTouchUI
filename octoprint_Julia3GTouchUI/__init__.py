e72281a4337bd19b094b297bf7c78c96ea06470c
Vijay Raghav Varada <vjvarada@fracktal.in>
2017-04-12 00:06:07 +0530

    bumped to 0.0.3

--- octoprint_Julia3GTouchUI/__init__.py
+++ octoprint_Julia3GTouchUI/__init__.py
@@ -5,7 +5,7 @@ import octoprint.plugin
 
 class Julia3GTouchUI(octoprint.plugin.StartupPlugin):
 	def on_after_startup(self):
-		self._logger.info("TouchUI PLugin Started")
+		self._logger.info("TouchUI Plugin Started")
 
 	def get_update_information(self):
 		return dict(
@@ -26,7 +26,7 @@ class Julia3GTouchUI(octoprint.plugin.StartupPlugin):
 
 
 __plugin_name__ = "Julia3GTouchUI"
-__plugin_version__ = "0.0.2"
+__plugin_version__ = "0.0.3"
 
 
 def __plugin_load__():

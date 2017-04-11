3ef89de981ac947399350212612a40eb2ecc8b86
Vijay Raghav Varada <vjvarada@fracktal.in>
2017-04-11 22:50:55 +0530

    Changed init.py

---
+++ octoprint_Julia3GTouchUI/__init__.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+class Julia3GTouchUI(octoprint.plugin.StartupPlugin):
+
+	def get_update_information(self):
+		return dict(
+			octoprint_Julia3GFilament=dict(
+				displayName="Julia3GTouchUI",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="FracktalWorks",
+				repo="Julia3GTouchUI",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/FracktalWorks/Julia3GTouchUI/archive/{target_version}.zip"
+			)
+		)
+
+
+__plugin_name__ = "Julia3GTouchUI"
+__plugin_version__ = "0.0.1"
+
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = Julia3GTouchUI()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+
+
